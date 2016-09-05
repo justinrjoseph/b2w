cb2d633fc57f5c9a81d91ae949048f5b6d2d8009
Justin Joseph <justin.joseph0@yahoo.com>
2016-09-05 12:56:19 -0400

    Convert home page video section to use advanced custom fields

--- wp-content/themes/b2w/page-home.php
+++ wp-content/themes/b2w/page-home.php
@@ -33,6 +33,11 @@ $features_section_body = get_field('features_section_body');
 $project_section_title = get_field('project_section_title');
 $project_section_description = get_field('project_section_description');
 
+$video_section_title = get_field('video_section_title');
+$video_url = get_field('video_url');
+$video_width = get_field('video_width');
+$video_height = get_field('video_height');
+
 get_header(); ?>
 
 		<!-- HERO 
@@ -246,8 +251,8 @@ get_header(); ?>
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-8 col-sm-offset-2">
-						<h2>Watch the Course Introduction</h2>
-						<iframe width="100%" height="415" src="https://www.youtube.com/embed/q-mJJsnOHew" frameborder="0" allowfullscreen></iframe>
+						<h2><?php echo $video_section_title; ?></h2>
+						<iframe width="<?php echo $video_width; ?>" height="<?php echo $video_height; ?>" src="<?php echo $video_url; ?>" frameborder="0" allowfullscreen></iframe>
 					</div><!-- /.col-sm-8 col-small-offset-2 -->
 				</div><!-- /.row -->
 			</div><!-- /.container -->
