cd5bc52f6c8a1ced8fd17893f21525daa80b2f60
Justin Joseph <justin.joseph0@yahoo.com>
2016-09-04 23:15:26 -0400

    Convert who should take this course section content to use advanced custom fields

--- wp-content/themes/b2w/page-home.php
+++ wp-content/themes/b2w/page-home.php
@@ -14,13 +14,17 @@ $cta_benefit = get_post_meta( 7, 'cta_benefit', true );
 $cta_btn_txt = get_post_meta( 7, 'cta_btn_txt', true );
 
 // Advanced custom fields
-$income_feature_image = get_field('income_feature_image');
+$income_featured_image = get_field('income_featured_image');
 $income_section_title = get_field('income_section_title');
 $income_section_description = get_field('income_section_description');
 $reason_1_title = get_field('reason_1_title');
 $reason_1_description = get_field('reason_1_description');
 $reason_2_title = get_field('reason_2_title');
 $reason_2_description = get_field('reason_2_description');
+
+$who_featured_image = get_field('who_featured_image');
+$who_section_title = get_field('who_section_title');
+$who_section_body = get_field('who_section_body');
 
 get_header(); ?>
 
@@ -96,8 +100,8 @@ get_header(); ?>
 			<div class="container">
 				
 				<div class="section-header">
-					<?php if ( !empty($income_feature_image) ) : ?>
-					<img src="<?php echo $income_feature_image['url']; ?>" alt="<?php echo $income_feature_image['alt']; ?>">
+					<?php if ( !empty($income_featured_image) ) : ?>
+					<img src="<?php echo $income_featured_image['url']; ?>" alt="<?php echo $income_featured_image['alt']; ?>">
 					<?php endif; ?>
 					<h2><?php echo $income_section_title; ?></h2>
 				</div><!-- /.section-header -->
@@ -130,49 +134,16 @@ get_header(); ?>
 			<div class="container">
 				
 				<div class="section-header">
-					<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/icon-pad.png" alt="Pad and pencil">
-					<h2>Who Should Take This Course?</h2>
+					<?php if ( !empty($who_featured_image) ) : ?>
+					<img src="<?php echo $who_featured_image['url']; ?>" alt="<?php echo $who_featured_image['alt']; ?>">
+					<?php endif; ?>
+					<h2><?php echo $who_section_title; ?></h2>
 				</div><!-- /.section-header -->
 
 				<div class="row">
 					<div class="col-sm-8 col-sm-offset-2">
 						
-						<h3>Graphics &amp; Web Designers</h3>
-						<p>
-							Graphic designers are extremely talented, but ask them to code their designs and they&rsquo;ll freeze up! This leaves them with no other choice but to hire a web developer. Any professional graphic designer knows web developers can be expensive.
-						</p>
-						<p>
-							If you&rsquo;re a designer, learning to code your own WordPress websites can change your business entirely! Now, not only are you a great designer, but a skilled developer too! This puts you in a position to <strong>make an extra $1,000 - $5,000 per project.</strong>
-						</p>
-
-						<h3>Entrepreneurs</h3>
-						<p>
-							Entrepreneurs have big dreams, and in many cases, shoe-string budgets. In order to survive in the cut-throat world of a startup company, it&rsquo;s a necessity to have a world-class website. However, world-class websites come with a large price tag.
-						</p>
-						<p>
-							If you can learn how to build a high-quality startup website by yourself, then you&rsquo;ve just saved yourself a lot of cash, <strong>tens of thousands of dollars in many cases</strong>.
-						</p>
-
-						<h3>Employees</h3>
-						<p>
-							Any company knows the education &amp; training of their employees is key to a thriving team.
-						</p>
-						<p>
-							Depending on the type of company you work for, if you understand how to code, and can develop CMS-driven websites, that gives you <strong>negotiating power for a better position, or a higher salary</strong>.
-						</p>
-
-						<h3>Code Hobbyists</h3>
-						<p>
-							It&rsquo;s fun to learn challenging, new skills. Code hobbyists can add dynamic websites to their arsenal of tools to play with &mdash; you can even <strong>sell WordPress themes and plugins for cash!</strong> The possibilities are truly endless.
-						</p>
-
-						<h3>People Looking for a New Career</h3>
-						<p>
-							Are you out of work? Looking for a more rewarding job? Desire a career that can allow you to work almost anywhere in the world? Becoming a web developer might be the answer for you.
-						</p>
-						<p>
-							<strong>Web developers are paid well, anywhere from $33,000 to more than $105,000 per year</strong>. They get to work at amazing companies that are changing the world, or they enjoy the ability to start their own companies, become location-independent and work from home, from coffee shops, in an airplane, on the beach, or wherever they want!
-						</p>
+						<?php echo $who_section_body; ?>
 
 					</div><!-- /.col-sm-8 col-sm-offset-2 -->
 				</div><!-- /.row -->
