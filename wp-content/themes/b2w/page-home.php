f975066e284be7ad2d7d32342e2edf9071d44b04
Justin Joseph <justin.joseph0@yahoo.com>
2016-09-04 22:24:38 -0400

    Convert home page hero content to use custom fields

--- wp-content/themes/b2w/page-home.php
+++ wp-content/themes/b2w/page-home.php
@@ -3,6 +3,16 @@
 	Template Name: Home Page
 */
 
+// Custom fields
+$prelaunch_price = get_post_meta( 7, 'prelaunch_price', true );
+$launch_price = get_post_meta( 7, 'launch_price', true );
+$final_price = get_post_meta( 7, 'final_price', true );
+$course_url = get_post_meta( 7, 'course_url', true );
+$enroll_btn_txt = get_post_meta( 7, 'enroll_btn_txt', true );
+$call_to_action = get_post_meta( 7, 'call_to_action', true );
+$cta_benefit = get_post_meta( 7, 'cta_benefit', true );
+$cta_btn_txt = get_post_meta( 7, 'cta_btn_txt', true );
+
 get_header(); ?>
 
 		<!-- HERO 
@@ -17,31 +27,31 @@ get_header(); ?>
 						</div><!-- /.col-sm-5 -->
 
 						<div class="col-sm-7 hero-text">
-							<h1>Bootstrap to WordPress</h1>
-							<p class="lead">Earn An Extra $1k - $5k a Month by Learning to Build Your Own Responsive &amp; Custom WordPress websites with Bootstrap</p><!-- /.lead -->
+							<h1><?php bloginfo( 'title' ) ?></h1>
+							<p class="lead"><?php bloginfo( 'description' ); ?></p><!-- /.lead -->
 							<div id="price-timeline">
 								<div class="price active">
 									<h4>
 										Pre-Launch Price <small>Ends Soon!</small>
 									</h4>
-									<span>$149</span>
+									<span><?php echo $prelaunch_price; ?></span>
 								</div><!-- /.price -->
 								<div class="price">
 									<h4>
 										Launch Price <small>Coming Soon!</small>
 									</h4>
-									<span>$299</span>
+									<span><?php echo $launch_price; ?></span>
 								</div><!-- /.price -->
 								<div class="price">
 									<h4>
 										Final Price <small>Coming Soon!</small>
 									</h4>
-									<span>$399</span>
+									<span><?php echo $final_price; ?></span>
 								</div><!-- /.price -->
 							</div><!-- /#price-timeline -->
 
 							<p>
-								<a href="/" class="btn btn-lg btn-danger" role="button">Enroll &raquo;</a>
+								<a href="<?php echo $course_url; ?>" class="btn btn-lg btn-danger" role="button"><?php echo $enroll_btn_txt; ?> &raquo;</a>
 							</p>
 						</div><!-- /.col-sm-7 hero-text -->
 
@@ -58,13 +68,13 @@ get_header(); ?>
 					
 					<div class="col-sm-8">
 						<p class="lead">
-							<strong>Subscribe to Our Mailing List:</strong>
-							We&rsquo;ll send something special as a thank-you.
+							<strong><?php echo $call_to_action; ?></strong>
+							<?php echo $cta_benefit; ?>
 						</p><!-- /.lead -->
 					</div><!-- /.col-sm-8 -->
 
 					<div class="col-sm-4">
-						<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#modal">Click Here to Subscribe</button>
+						<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#modal"><?php echo $cta_btn_txt; ?></button>
 					</div><!-- /.col-sm-4 -->
 
 				</div><!-- /.row -->
