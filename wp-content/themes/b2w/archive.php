c0a6f2e415c039e09731e1f8f603ee7ca811b45a
Justin Joseph <justin.joseph0@yahoo.com>
2016-09-05 17:46:54 -0400

    Customize the archive template

--- wp-content/themes/b2w/archive.php
+++ wp-content/themes/b2w/archive.php
@@ -9,43 +9,43 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<?php if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+	<!-- FEATURE IMAGE 
+	=========================================================== -->
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1>
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+				the_archive_title();
+				the_archive_description( '<small class="taxonomy-description">', '</small>' );
+			?>
+		</h1>
+	</section>
+	
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			endwhile;
+				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-			the_posts_navigation();
+			<?php endwhile; ?>
 
-		else :
+	<?php else : ?>
 
-			get_template_part( 'template-parts/content', 'none' );
+		<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		endif; ?>
+	<?php endif; ?>
+			</main><!-- /#content.col-sm-8 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<!-- SIDEBAR 
+			=========================================================== -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside><!-- /.col-sm-4 -->
 
-<?php
-get_sidebar();
-get_footer();
+		</div><!-- /#primary.row -->
+	</div><!-- /.container -->
+
+<?php get_footer(); ?>
